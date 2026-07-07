e7f032120684e61e85194808970a0c897422bccb
A <l2408849830as@gmail.com>
2026-07-07 18:52:22 +0900

    第10回課題

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,13 +1,10 @@
 package raisetech.StudentManagement;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.mybatis.spring.annotation.MapperScan;
 
@@ -22,26 +19,12 @@ public class StudentManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
-	@GetMapping("/Info")
-	public String getInfo(@RequestParam String name) {
-		Student student = repository.searchByName(name);
-		return student.getName() + " " + student.getAge() + "歳";
-
+	@GetMapping("/student")
+	public List<Student> getStudentinfo() {
+		return repository.search();
 	}
-
-	@PostMapping("/Info")
-	public void registerInfo(String name , int age) {
-		repository.registerStudent(name, age);
+	@GetMapping("/student_courses")
+	public List<Student_courses> getStudent_Courses() {
+		return repository.searchStudent_courses();
 	}
-
-	@PatchMapping("/Info")
-	public void updateInfo(String name, int age) {
-		repository.updateInfo(name, age);
-	}
-
-	@DeleteMapping("/Info")
-	public void deleteInfo(String name) {
-		repository.deleteInfo(name);
-	}
-
 }
--- src/main/java/raisetech/StudentManagement/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -1,25 +1,17 @@
 package raisetech.StudentManagement;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
+  @Select("SELECT * FROM student")
+  List<Student> search();
 
-  @Insert("INSERT student values(#{name},#{age})")
-  void registerStudent(String name , int age);
-
-  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-  void updateInfo(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteInfo(String name);
+  @Select("SELECT * FROM student_courses")
+  List<Student_courses> searchStudent_courses() ;
 }
